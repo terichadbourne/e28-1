3c3cc5ec2991a29964d59e7241e412e20276cddc
Susan Buck <susan@susanbuck.net>
2019-09-26 18:16:26 -0400

    Updated p1 for an example; Week 4 practice code

--- intro/script.js
+++ intro/script.js
@@ -2,7 +2,25 @@ let app = new Vue({
     el: '#app',
     data: {
         'playerName': null,
-        'guess': 'initial',
-        'guesses': []
+        'guess': null,
+        'guesses': [],
+        'results': null,
+        'settings': {
+            'max': 200,
+            'type': 'numeric',
+            'guessLimit': 15
+        },
+        'answer': 55,
+        'guessesDetailed': [
+            { guess: 15, result: 'low' },
+            { guess: 60, result: 'high' },
+            { guess: 50, result: 'low' },
+        ]
+
     },
+    methods: {
+        submitGuess() {
+            this.guesses.push(this.guess);
+        },
+    }
 });

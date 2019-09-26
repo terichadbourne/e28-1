2415c8b639e7513cb89ac311f9e82f7bbba37245
Susan Buck <susan@susanbuck.net>
2019-09-26 19:23:02 -0400

    Week 4 code clean-up

--- intro/script.js
+++ intro/script.js
@@ -1,22 +1,21 @@
 let app = new Vue({
     el: '#app',
     data: {
-        'playerName': null,
-        'guess': null,
-        'guesses': [],
-        'results': null,
-        'settings': {
-            'max': 200,
-            'type': 'numeric',
-            'guessLimit': 15
+        playerName: null,
+        guess: null,
+        guesses: [],
+        results: null,
+        settings: {
+            max: 200,
+            type: 'numeric',
+            guessLimit: 15
         },
-        'answer': 55,
-        'guessesDetailed': [
+        answer: 55,
+        guessesDetailed: [
             { guess: 15, result: 'low' },
             { guess: 60, result: 'high' },
             { guess: 50, result: 'low' },
         ]
-
     },
     methods: {
         submitGuess() {
